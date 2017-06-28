c0387f7efd9b040f2b19d5615b9f7360d0965936
Nathan Elg <elgn@students.wwwu.edu>
2017-06-28 16:35:09 -0600

    css changes

--- www/src/components/Vault.vue
+++ www/src/components/Vault.vue
@@ -16,7 +16,7 @@
                   aria-hidden="true"> {{keep.viewCount || 0}}</i></p>
               <h4 class="card-title">{{keep.title}}</h4>
               <div id="keep-buttons" class="btn-group" role="group" aria-label="Basic example">
-                <button id="keep-button" type="button" data-toggle="modal" data-target="#keepModal" class="btn btn-secondary">K</button>
+                <!--<button id="keep-button" type="button" data-toggle="modal" data-target="#keepModal" class="btn btn-secondary">K</button>-->
                 <button id="share-button" type="button" class="btn btn-secondary"><i class="fa fa-share" aria-hidden="true"></i></button>
                 <button id="view-button"  type="button" class="btn btn-secondary"><i class="fa fa-eye"
                   aria-hidden="true"></i></button>
@@ -63,5 +63,22 @@ export default {
 
 
 <style scoped>
+ #share-button{
+    background-color: black;
+    color: white;
+    margin: 4px;
+  }
+  #view-button{
+    background-color: purple;
+    color: white;
+    margin: 4px;
+  }
+  #delete-button{
+    color: white;
+    margin: 4px;
+  }
+  .card:hover {
+    background-color: #eee;
+  }
 
 </style>
