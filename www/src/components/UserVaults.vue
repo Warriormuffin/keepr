0b30a172a358390f3e2d53944aaee476b8c78e6f
Nathan Elg <elgn@students.wwwu.edu>
2017-06-28 18:12:23 -0600

    i did things

--- www/src/components/UserVaults.vue
+++ www/src/components/UserVaults.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="uservaults">
     <div class="row">
-      <div class="card-columns">
-        <div v-for="vault in myVaults">
-          <div class="card text-center">
-            <div class="card-block">
-              <h4 class="card-title">{{vault.title}}</h4>
-              <p class="card-text">{{vault.description}}</p>
-              <div id="keep-buttons" class="btn-group" role="group" aria-label="Basic example">
-                <router-link :to="'/vault/' + vault._id"><button id="view-button" type="button" class="btn btn-secondary"><i class="fa fa-eye" aria-hidden="true"></i></button></router-link>
-                <button @click="deleteVault(vault._id)" id="delete-button" type="button" class="btn btn-danger">
-                  <i class="fa fa-trash" aria-hidden="true"></i>
-                </button>
+          <div class="card-columns">
+          <div v-for="vault in myVaults">
+            <div class="card text-center">
+              <div class="card-block">
+                <h4 class="card-title">{{vault.title}}</h4>
+                <p class="card-text">{{vault.description}}</p>
+                <div id="keep-buttons" class="btn-group" role="group" aria-label="Basic example">
+                  <router-link :to="'/vault/' + vault._id">
+                    <button id="view-button" type="button" class="btn btn-secondary">
+                      <i class="fa fa-eye" aria-hidden="true"></i>
+                    </button>
+                  </router-link>
+                  <button @click="deleteVault(vault._id)" id="delete-button" type="button" class="btn btn-danger">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 </template>
 
 
@@ -30,7 +34,7 @@ export default {
 
     }
   },
-  mounted(){
+  mounted() {
     this.$store.dispatch("getMyVaults")
   },
   computed: {
@@ -40,7 +44,7 @@ export default {
     }
   },
   methods: {
-    deleteVault(id){
+    deleteVault(id) {
       this.$store.dispatch('deleteVault', id)
     },
 
@@ -63,5 +67,9 @@ export default {
 #delete-button {
   color: white;
   margin: 4px;
+}
+
+.card:hover {
+  background-color: #eee;
 }
 </style>
